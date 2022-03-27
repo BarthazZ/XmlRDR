7545967927eb412046bd184933b15037ff9d2c66
Bartłomiej Martenka <94762027+BarthazZ@users.noreply.github.com>
2022-03-27 21:14:15 +0200

    Add project files.

---
+++ XmlRDR/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Data;
+using System.Xml;
+
+namespace XmlRDR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(@"C:\Users\Patryk\Desktop\Oferty Pracy\oferta_pracy_SPRZEDAWCA.xml");
+
+            XmlNodeList nodes = doc.SelectNodes("//SzczegolyOferty/danePracodawcy");
+
+            foreach (XmlNode node in nodes)
+            {
+                XmlNode adres = node.SelectSingleNode("adres");
+                if (adres != null)
+                {
+                    Console.WriteLine(@"Adres:" + adres.InnerText);
+                }
+
+                XmlNode jezykAplikacji = node.SelectSingleNode("jezykAplikacji");
+                if (jezykAplikacji != null)
+                {
+                    Console.WriteLine(@"Język Aplikacji:" + jezykAplikacji.InnerText);
+                }
+            } 
+        }    
+    
+    }
+}
